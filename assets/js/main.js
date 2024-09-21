ad85f3cb69d30b81dab34fd917e3a6db76523584
Igor Kodzhebash <igorkodzebash@gmail.com>
2024-09-21 17:23:10 +0300

    feat: done tracks list rendering

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import { data } from './data.js';
+import { handleTime } from './utils.js';
 
 const AudioController = {
   state: {
@@ -11,6 +12,34 @@ const AudioController = {
 
   initVariables() {
     this.audioList = document.querySelector('.app__tracks');
+    this.audioList.innerHTML = '';
+  },
+
+  renderItem({ id, track, group, genre, link, duration }) {
+    const [image] = link.split('.');
+    const time = handleTime(duration);
+
+    return `
+       <li class="app__track track" data-id="${id}">
+
+            <img class="track__image" src="./assets/images/${image}.jpg">
+            <div class="track__heading">
+              <span class="track__title">${track}</span>
+              <span class="track__author">${group}</span>
+            </div>
+
+            <div class="track__genre">${genre}</div>
+            <div class="track__duration">${time}</div>
+            <svg class="app__icon app__icon-small play">
+              <use xlink:href="./assets/icons/sprite.svg#play-solid"></use>
+            </svg>
+
+        </li>
+    `;
+  },
+
+  loadTracks(item) {
+    this.audioList.innerHTML += this.renderItem(item);
   },
 
   renderAudios() {
@@ -21,16 +50,10 @@ const AudioController = {
         const newItem = { ...item, duration: audio.duration, audio };
 
         this.state.audios = [...this.state.audios, newItem];
-
-        console.log(this.state);
+        this.loadTracks(newItem);
       });
     });
   },
 };
 
 AudioController.intit();
-
-const appButtons = document.querySelector('.app__buttons');
-document.addEventListener('scroll', () => {
-  console.log(window.client);
-});
